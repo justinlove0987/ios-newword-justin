179365077ad28745863d9817d6858c21abfe76e8
Justin <justinlove0987@gmail.com>
2024-04-19 15:42:50 +0800

    feat: update model

--- NewWord/Models/Card.swift
+++ NewWord/Models/Card.swift
@@ -17,18 +17,41 @@ struct Card {
 }
 
 extension Card {
+
     enum CardState {
         case new
         case relearn
         case review
     }
     
-    var reviews: Int { learningRecords.count }
-    
+    var reviews: Int {
+        return learningRecords
+            .filter { $0.state != .relearn }
+            .count
+    }
+
     var hasReivews: Bool { return learningRecords.count != 0 }
-    
+
+
+    /// This is the state existing **prior to** the answer card.
     var cardState: CardState {
-        return hasReivews ? .review : .new
+        if !hasReivews {
+            return .new
+        }
+
+        let learningState = latestReview!.state
+        let learningStatus = latestReview!.status
+
+        switch (learningState, learningStatus) {
+        case (.learn, .incorrect):
+            return .new
+        case (.learn, .correct), (.relearn, .correct), (.review, .correct):
+            return .review
+        case (.relearn, .incorrect), (.review, .incorrect):
+            return .relearn
+        }
+
+
     }
     
     var firstReview: LearningRecord? {
@@ -46,20 +69,21 @@ extension Card {
     }
     
     var firstReviewDate: Date? {
-        return firstReview?.createdDate
+        return firstReview?.learnedDate
     }
     
     var lastestReviewDate: Date? {
-        return latestReview?.createdDate
+        return latestReview?.learnedDate
     }
     
-    var interval: Int? {
+    /// The unit of interval is **second**.
+    var interval: Double? {
         return latestReview?.interval
     }
     
     var lapses: Int {
         return learningRecords.reduce(0) { partialResult, record in
-            return record.status == .incorrect ? (partialResult + 1) : partialResult
+            return (record.state == .relearn && record.status == .incorrect) ? (partialResult + 1) : partialResult
         }
     }
 }
@@ -82,13 +106,13 @@ extension Card {
         let date2 = Card.dateCreator(year: 2024, month: 5, day: 2)
         let date3 = Card.dateCreator(year: 2024, month: 5, day: 3)
 
-        let card1 = Card(id: UUID().uuidString, note: note1, learningRecords: [LearningRecord(createdDate: date1, dueDate: date1, interval: 1, status: .correct),
-                                                                               LearningRecord(createdDate: date2, dueDate: date2, interval: 2, status: .correct),
-                                                                               LearningRecord(createdDate: date3, dueDate: date3, interval: 3, status: .correct),
+        let card1 = Card(id: UUID().uuidString, note: note1, learningRecords: [LearningRecord(learnedDate: date1, dueDate: date1, status: .correct, state: .relearn),
+                                                                               LearningRecord(learnedDate: date2, dueDate: date2, status: .correct, state: .learn),
+                                                                               LearningRecord(learnedDate: date3, dueDate: date3, status: .correct, state: .relearn),
                                                                               ])
-        let card2 = Card(id: UUID().uuidString, note: note2, learningRecords: [LearningRecord(createdDate: date1, dueDate: date1, interval: 1, status: .correct),
-                                                                               LearningRecord(createdDate: date2, dueDate: date2, interval: 2, status: .correct),
-                                                                               LearningRecord(createdDate: date3, dueDate: date3, interval: 3, status: .correct),
+        let card2 = Card(id: UUID().uuidString, note: note2, learningRecords: [LearningRecord(learnedDate: date1, dueDate: date1, status: .correct, state: .relearn),
+                                                                               LearningRecord(learnedDate: date2, dueDate: date2, status: .correct, state: .review),
+                                                                               LearningRecord(learnedDate: date3, dueDate: date3, status: .correct, state: .review),
                                                                               ])
 
         return [card1, card2]
--- NewWord/Models/LearningRecord.swift
+++ NewWord/Models/LearningRecord.swift
@@ -10,15 +10,10 @@ import Foundation
 
 struct LearningRecord {
 
-    enum TimeUnit {
-        case minute
-        case hour
-        case day
-
-        static let secondsInAMinute = 60
-        static let secondsInAnHour = 60 * 60
-        static let secondsInADay = 60 * 60 * 24
-        static let secondsInAMonth = 2592000
+    enum State {
+        case learn
+        case relearn
+        case review
     }
 
     enum Status {
@@ -26,25 +21,20 @@ struct LearningRecord {
         case incorrect
     }
     
-    let createdDate: Date
+    let learnedDate: Date
     let dueDate: Date
-    let interval: Int
     let totalTime: Int = 0
     let status: Status
+
+    /// This is the state **after** the answer card.
+    let state: State
 }
 
 extension LearningRecord {
-    func getInterval(timeUnit: TimeUnit) -> Int {
-        switch timeUnit {
-        case .minute:
-            let minutes = interval / TimeUnit.secondsInAMinute
-            return interval % TimeUnit.secondsInADay == 0 ? minutes : minutes + 1
-
-        case .hour:
-            return interval / TimeUnit.secondsInAnHour
-        case .day:
-            return interval / TimeUnit.secondsInADay
-        }
+    
+    /// The unit of interval is **second**.
+    var interval: Double {
+        return dueDate.timeIntervalSince(learnedDate) // seconds
     }
 
     static func countDueDate() -> Date {
