da250b038b6cb48d7eb4993b1101711de7029a7c
Justin <justinlove0987@gmail.com>
2024-06-04 14:29:53 +0800

    feat: add addClozeViewController

---
+++ NewWord/Controllers/AddClozeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddClozeViewController.swift
+//  NewWord
+//
+//  Created by 曾柏楊 on 2024/6/4.
+//
+
+import UIKit
+
+class AddClozeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
