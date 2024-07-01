9a9b14af25cddbf3f703986ce390408eced94975
Justin <justinlove0987@gmail.com>
2024-07-01 16:20:14 +0800

    feat: add number to cloze

--- NewWord/Controllers/NewAddClozeViewController.swift
+++ NewWord/Controllers/NewAddClozeViewController.swift
@@ -9,45 +9,55 @@ import UIKit
 import NaturalLanguage
 
 class NewAddClozeViewController: UIViewController, StoryboardGenerated {
-
+    
+    struct NewAddCloze {
+        let number: Int
+        let cloze: String
+        let range: NSRange
+    }
+    
     static var storyboardName: String = K.Storyboard.main
-
+    
     @IBOutlet weak var textView: UITextView!
     private var customTextView: AddClozeTextView!
-
+    
+    private var clozeNumbers: Set<Int> = .init()
+    
+    private var clozes: [NewAddCloze] = []
+    
     // MARK: - Lifecycles
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTextView()
         setupCumstomTextView()
     }
-
+    
     // MARK: - Helpers
-
+    
     private func setupTextView() {
         textView.isEditable = false
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         textView.addGestureRecognizer(tapGesture)
         textView.isHidden = true
     }
-
+    
     private func setupCumstomTextView() {
         let attributedString = NSMutableAttributedString(string: textView.text)
-
+        
         // 創建一個 Text Storage 和 Layout Manager
         let textStorage = NSTextStorage(attributedString: attributedString)
         let layoutManager = NSLayoutManager()
         textStorage.addLayoutManager(layoutManager)
-
+        
         // 創建一個 Text Container 並配置它
         let textContainer = NSTextContainer(size: CGSize(width: textView.frame.width, height: textView.frame.height))
         textContainer.lineFragmentPadding = 0
         layoutManager.addTextContainer(textContainer)
-
+        
         // 創建一個自定義 UITextView 並配置它
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-
+        
         customTextView = AddClozeTextView(frame: textView.frame, textContainer: textContainer)
         customTextView.isEditable = false
         customTextView.isScrollEnabled = true
@@ -57,9 +67,9 @@ class NewAddClozeViewController: UIViewController, StoryboardGenerated {
         customTextView.translatesAutoresizingMaskIntoConstraints = false
         customTextView.addGestureRecognizer(tapGesture)
         customTextView.delegate = self
-
+        
         self.view.addSubview(customTextView)
-
+        
         NSLayoutConstraint.activate([
             customTextView.topAnchor.constraint(equalTo: textView.topAnchor),
             customTextView.bottomAnchor.constraint(equalTo: textView.bottomAnchor),
@@ -67,57 +77,137 @@ class NewAddClozeViewController: UIViewController, StoryboardGenerated {
             customTextView.trailingAnchor.constraint(equalTo: textView.trailingAnchor),
         ])
     }
-
+    
     // MARK: - Actions
-
-
+    
+    
     @IBAction func segmentedAction(_ sender: UISegmentedControl) {
     }
-
-
+    
+    
     @IBAction func saveAction(_ sender: Any) {
+        
+        saveCloze()
     }
-
+    
     @objc func handleTap(_ gesture: UITapGestureRecognizer) {
         guard let customTextView = gesture.view as? AddClozeTextView else { return }
         var location = gesture.location(in: customTextView)
-
+        
         location.x -= customTextView.textContainerInset.left
         location.y -= customTextView.textContainerInset.top
-
+        
         if let characterIndex = customTextView.characterIndex(at: location),
            let wordRange = customTextView.wordRange(at: characterIndex) {
-
+            
             // 獲取點擊的單字
             let word = (customTextView.text as NSString).substring(with: wordRange)
             print("Tapped word: \(word)")
-
+            
             // 判斷點擊的單字是否有藍色背景
-            if customTextView.hasBlueBackground(at: wordRange) {
-                print("The word has a blue background.")
-            } else {
-                print("The word does not have a blue background.")
-            }
-
-            // 在單字前插入"1"
+            //            if customTextView.hasBlueBackground(at: wordRange) {
+            //                print("The word has a blue background.")
+            //            } else {
+            //                print("The word does not have a blue background.")
+            //            }
+            
+            let clozeNumber = getClozeNumbre()
+            
             let mutableAttributedString = NSMutableAttributedString(attributedString: customTextView.attributedText)
-            mutableAttributedString.insert(NSAttributedString(string: "1"), at: wordRange.location)
+            let yellowOne = NSMutableAttributedString(string: String(clozeNumber),
+                                                      attributes: [
+                                                        .backgroundColor: UIColor.clozeBlueNumber,
+                                                        .font: UIFont.systemFont(ofSize: 14, weight: .medium),
+                                                        // .kern: 2, // 調整數字1與後面字符的間距
+                                                      ])
+            
+            mutableAttributedString.insert(yellowOne, at: wordRange.location)
             customTextView.attributedText = mutableAttributedString
-
+            
             // 更新highlight範圍包括"1"
-            let updatedWordRange = NSRange(location: wordRange.location, length: wordRange.length + 1)
-
-
+            let updatedWordRange = NSRange(location: wordRange.location + String(clozeNumber).count, length: wordRange.length)
+            
+            
+            clozes.append(NewAddCloze(number: clozeNumber, cloze: word, range: updatedWordRange))
+            
             customTextView.highlightRange(updatedWordRange)
         }
-
+        
         // 檢查是否有文字被反白選中
         if let _ = customTextView.selectedTextRange {
             // 取消文字的反白選中狀態
             customTextView.selectedTextRange = nil
         }
     }
+    
+    func convertToContext(_ text: String, _ cloze: NewAddCloze) -> String {
+        let attributedText = NSMutableAttributedString(string: text)
+        let frontCharacter = NSAttributedString(string: "{{C")
+        let middleColon = NSAttributedString(string: "\(cloze.number):")
+        let backCharacter = NSAttributedString(string: "}}")
 
+        let backIndex = cloze.range.location + cloze.range.length-1
+        let frontIndex = cloze.range.location-1
+        let middleColonIndex = frontIndex + String(cloze.number).count-1
+        
+        attributedText.insert(backCharacter, at: backIndex)
+        attributedText.insert(middleColon, at: middleColonIndex)
+        attributedText.insert(frontCharacter, at: frontIndex)
+        
+        return attributedText.string
+    }
+    
+    func getClozeNumbre() -> Int {
+        if clozeNumbers.isEmpty {
+            clozeNumbers.insert(1)
+            return 1
+        }
+        
+        var maxClozeNumber = clozeNumbers.max()
+        
+        maxClozeNumber! += 1
+        
+        if clozeNumbers.contains(maxClozeNumber!) {
+            while clozeNumbers.contains(maxClozeNumber! + 1) {
+                maxClozeNumber! += 1
+            }
+        }
+        
+        clozeNumbers.insert(maxClozeNumber!)
+        
+        return maxClozeNumber!
+    }
+    
+    private func saveCloze() {
+        guard var text = textView.text else { return }
+
+        
+        let firstDeck = CoreDataManager.shared.getDecks().first!
+        
+        for cloze in clozes {
+            let number = cloze.number
+            text = convertToContext(text, cloze)
+        }
+        
+        let context = CoreDataManager.shared.createContext(text)
+        
+        for cloze in clozes {
+            let cloze = CoreDataManager.shared.createCloze(number: cloze.number, hint: "", clozeWord: cloze.cloze)
+            cloze.context = context
+            cloze.contextId = context.id
+            
+            let noteType = CoreDataManager.shared.createNoteNoteType(rawValue: 1)
+            noteType.cloze = cloze
+            
+            let note = CoreDataManager.shared.createNote(noteType: noteType)
+            
+            CoreDataManager.shared.addCard(to: firstDeck, with: note)
+        }
+
+        CoreDataManager.shared.save()
+
+        navigationController?.popViewController(animated: true)
+    }
 }
 
 extension UITextView {
@@ -169,7 +259,7 @@ class AddClozeTextView: UITextView {
                 let adjustedRect = boundingRect.offsetBy(dx: self.textContainerInset.left, dy: self.textContainerInset.top)
 
                 // 設置背景顏色和圓角
-                let path = UIBezierPath(roundedRect: adjustedRect.insetBy(dx: -2, dy: -2), cornerRadius: 5)
+                let path = UIBezierPath(roundedRect: adjustedRect.insetBy(dx: 0, dy: 0), cornerRadius: 5)
                 UIColor.blue.setFill()
                 path.fill()
 
@@ -191,7 +281,7 @@ class AddClozeTextView: UITextView {
 
     func highlightRange(_ range: NSRange) {
         highlightedRange = range
-        textStorage.addAttribute(.backgroundColor, value: UIColor.blue, range: range)
+        textStorage.addAttribute(.backgroundColor, value: UIColor.clozeBlueText, range: range)
         setNeedsDisplay()
     }
 
@@ -211,8 +301,6 @@ extension NewAddClozeViewController: UITextViewDelegate {
             let end = textView.offset(from: textView.beginningOfDocument, to: selectedRange.end)
             let selectedText = (textView.text as NSString).substring(with: NSRange(location: start, length: end - start))
 
-            print("Selected text: \(selectedText)")
-            print("Selected range: \(selectedRange)")
         }
     }
 }
---
+++ NewWord/ViewModels/NewAddClozeViewControllerViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NewAddClozeViewControllerViewModel.swift
+//  NewWord
+//
+//  Created by 曾柏楊 on 2024/7/1.
+//
+
+import Foundation
+
+
+struct NewAddClozeViewControllerViewModel {
+    
+}
