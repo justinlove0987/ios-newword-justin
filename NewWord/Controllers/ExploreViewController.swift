0cd6f8be8129dd5706da5980b2d354ffa10d453a
Justin <justinlove0987@gmail.com>
2024-08-15 08:02:29 +0800

    feat: add ssml service

--- NewWord/Controllers/ExploreViewController.swift
+++ NewWord/Controllers/ExploreViewController.swift
@@ -26,7 +26,10 @@ class ExploreViewController: UIViewController, StoryboardGenerated {
         super.viewDidLoad()
         setup()
         fetchArticles()
-//         uploadArticles()
+//        uploadArticles()
+        
+        //        GoogleTTSService.shared.playAudioWithMarks()
+        
     }
     
     private func setup() {
@@ -41,41 +44,20 @@ class ExploreViewController: UIViewController, StoryboardGenerated {
                 newArticles.append(article)
             }
             
-            print("foo - \(newArticles)")
-            
             self.articles = newArticles
         }
     }
-
+    
     private func uploadArticles() {
-        let articles: [FSArticle] = [
-            FSArticle(
-                title: "The Impact of Climate Change on Global Agriculture",
-                content: """
-                Climate change is increasingly affecting global agriculture, posing challenges to food security and rural livelihoods. Rising temperatures, shifting precipitation patterns, and more frequent extreme weather events are disrupting crop yields and livestock production, leading to potential food shortages and increased prices.
-
-                One of the most significant impacts of climate change on agriculture is the alteration of growing seasons. In many regions, warmer temperatures are causing crops to mature more quickly, which can lead to lower yields as plants have less time to develop. Additionally, changes in rainfall patterns are creating drought conditions in some areas, while others may experience excessive rainfall, both of which can damage crops and reduce productivity.
-
-                Climate change also exacerbates the spread of pests and diseases. Warmer temperatures and increased humidity provide favorable conditions for the proliferation of insects, fungi, and bacteria that can devastate crops. This presents a particular challenge for farmers who must adapt to new threats and implement more resilient agricultural practices.
-
-                Livestock production is similarly affected by climate change. Heat stress can reduce the productivity of animals, impacting milk production, growth rates, and reproduction. Furthermore, the availability of feed and water resources may be compromised by changing environmental conditions, leading to increased costs and reduced output.
-
-                To address these challenges, researchers and policymakers are advocating for the adoption of climate-smart agricultural practices. These include the development of drought-resistant crop varieties, improved water management techniques, and the diversification of farming systems to enhance resilience. Additionally, efforts to reduce greenhouse gas emissions from agriculture, such as improving soil health and promoting sustainable land use practices, are crucial for mitigating the long-term impacts of climate change on global food systems.
-
-                As the global population continues to grow, ensuring the sustainability and resilience of agriculture in the face of climate change is essential for maintaining food security and supporting rural communities worldwide.
-                """,
-                imageId: UUID().uuidString,
-                uploadedDate: Date()
-            )
-        ]
-
+        let articles: [FSArticle] = []
+        
         FirestoreManager.shared.uploadArticles(articles) { uploaded in
             if uploaded {
                 print("foo - uploaded")
             }
         }
     }
-
+    
     private func setupCollectionView() {
         collectionView.register(UINib(nibName: ExploreCell.reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: ExploreCell.reuseIdentifier)
         dataSource = createDataSource()
@@ -87,14 +69,14 @@ class ExploreViewController: UIViewController, StoryboardGenerated {
     
     private func createDataSource() -> UICollectionViewDiffableDataSource<Int, FSArticle> {
         let dataSource = UICollectionViewDiffableDataSource<Int, FSArticle>(collectionView: collectionView,
-                                                                      cellProvider: { collectionView, indexPath, itemIdentifier in
+                                                                            cellProvider: { collectionView, indexPath, itemIdentifier in
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ExploreCell.reuseIdentifier, for: indexPath) as! ExploreCell
-
+            
             let currentArticle = self.articles[indexPath.row]
-
+            
             cell.updateUI(currentArticle)
             cell.imageDidSetCallback = { self.articles[indexPath.row].image = $0 }
-
+            
             return cell
         })
         
@@ -137,9 +119,9 @@ class ExploreViewController: UIViewController, StoryboardGenerated {
 extension ExploreViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let controller = ServerProvidedArticleViewController.instantiate()
-
+        
         controller.article = articles[indexPath.row]
-
+        
         navigationController?.pushViewControllerWithCustomTransition(controller)
     }
 }
--- NewWord/Utils/GoogleTTSService.swift
+++ NewWord/Utils/GoogleTTSService.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import AVFoundation
+import NaturalLanguage
 
 enum VoiceType: String {
     case undefined
@@ -42,6 +43,11 @@ class GoogleTTSService: NSObject {
     var finishCallback: (() -> Void)?
     var stopCallback: (() -> ())?
     var startCallback: (() ->())?
+    
+    var text = """
+    The sun dipped below the horizon, casting a golden hue over the tranquil sea. Gentle waves lapped at the shore, creating a soothing rhythm that matched the peaceful evening.
+    """
+
     
     func speak(_ audioData: Data?) {
         guard !self.busy else {
@@ -175,57 +181,65 @@ class GoogleTTSService: NSObject {
         return nil
     }
     
-    func fetchAndSpeak(text: String, voiceType: VoiceType = .waveNetMale, completion: @escaping (Data?) -> Void) {
-        guard !self.busy else {
-            print("Speech Service busy!")
-            return
-        }
-        
-        self.busy = true
-        
-        DispatchQueue.global(qos: .background).async {
-            let postData = self.buildPostData(text: text, voiceType: voiceType)
-            let headers = ["X-Goog-Api-Key": K.API.key, "Content-Type": "application/json; charset=utf-8"]
-            let response = self.makePOSTRequest(url: K.API.tts, postData: postData, headers: headers)
+    func playAudioWithMarks() {
+        downloadSSML { result in
+            guard let result else { return }
             
-            // Get the `audioContent` (as a base64 encoded string) from the response.
-            guard let audioContent = response["audioContent"] as? String else {
-                print("Invalid response: \(response)")
-                self.busy = false
-                DispatchQueue.main.async {
-                    completion(nil)
+            do {
+                self.player = try AVAudioPlayer(data: result.audioData)
+                self.player?.play()
+                
+                Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { [weak self] timer in
+                    guard let self = self, let player = self.player else {
+                        timer.invalidate()
+                        return
+                    }
+                    
+                    let currentTimeInSeconds = roundToOneDecimalPlace(player.currentTime)
+                    
+                    for timepoint in result.timepoints {
+                        
+                        let markName = timepoint.markName
+                        let markTime = roundToOneDecimalPlace(timepoint.timeSeconds)
+                        
+                        if markTime == currentTimeInSeconds && !currentTimeInSeconds.isZero  {
+                            if let nsRange = timepoint.range, let range = Range(nsRange, in: self.text) {
+                                let substring = text[range]
+                                print("Selected text: \(substring) \(nsRange) \(currentTimeInSeconds)")
+                            } else {
+                                print("Invalid range")
+                            }
+                        }
+                    }
+                    
+                    // 如果音頻播放結束則停止計時器
+                    if !player.isPlaying {
+                        timer.invalidate()
+                    }
                 }
-                return
+                
+                
+            } catch {
+                
             }
             
-            // Decode the base64 string into a Data object
-            guard let audioData = Data(base64Encoded: audioContent) else {
-                self.busy = false
-                DispatchQueue.main.async {
-                    completion(nil)
-                }
-                return
-            }
             
-            DispatchQueue.main.async {
-                completion(audioData)
-                self.busy = false
-            }
         }
     }
 
-    private func trySSML(voiceType: VoiceType = .waveNetMale, completion: @escaping (Data?) -> Void) {
-        let text =
-            """
-<speak>Hello <mark name="timepoint_1"/> Mark. Good to <mark
-name="timepoint_2"/> see you.</speak>
-"""
-
+    func downloadSSML(voiceType: VoiceType = .waveNetMale, completion: @escaping (TTSSynthesisResult?) -> Void) {
+        var text = addMarksToText(text)
+        text = wrapWithSpeakTags(text)
+        
         downloadQueue.async {
-            let postData = self.buildPostData(text: text, voiceType: voiceType)
+            let postData = self.buildPostData(text: text,
+                                              synthiesisInput: .ssml,
+                                              timepointType: .SSML_MARK,
+                                              voiceType: voiceType)
+            
             let headers = ["X-Goog-Api-Key": K.API.key, "Content-Type": "application/json; charset=utf-8"]
             let response = self.makePOSTRequest(url: K.API.tts, postData: postData, headers: headers)
-
+            
             // 獲取 `audioContent` 並解碼
             guard let audioContent = response["audioContent"] as? String,
                   let audioData = Data(base64Encoded: audioContent) else {
@@ -234,11 +248,91 @@ name="timepoint_2"/> see you.</speak>
                 }
                 return
             }
-
+            
+            // 解析 timepoints 並封裝到 TimepointInfo 中
+            var timepoints: [TimepointInfo] = []
+            
+            if let tpArray = response["timepoints"] as? [[String: Any]] {
+                for tp in tpArray {
+                    if let markName = tp["markName"] as? String,
+                       let timeSeconds = tp["timeSeconds"] as? Double {
+                        // 解析 markName 並生成 NSRange
+                        let components = markName.split(separator: "_")
+                        let range: NSRange? = {
+                            if components.count == 3,
+                               let location = Int(components[1]),
+                               let length = Int(components[2]) {
+                                return NSRange(location: location, length: length)
+                            }
+                            return nil
+                        }()
+                        let timepointInfo = TimepointInfo(range: range, markName: markName, timeSeconds: timeSeconds)
+                        timepoints.append(timepointInfo)
+                    }
+                }
+            }
+            
+            // 封裝到 TTSSynthesisResult 結構體中
+            let result = TTSSynthesisResult(audioData: audioData, timepoints: timepoints)
             DispatchQueue.main.async {
-                completion(audioData)
+                completion(result)
             }
         }
+    }
+    
+    
+    func addMarksToText(_ text: String) -> String {
+        let tokenizer = NLTokenizer(unit: .word)
+        
+        var markedText = ""
+        var currentPosition = 0
+        var previousWordRange: Range<String.Index>? = nil
+        
+        tokenizer.string = text
+        
+        tokenizer.enumerateTokens(in: text.startIndex..<text.endIndex) { (range, _) in
+            let word = text[range]
+            let wordLength = word.count
+            
+            if let previousWordRange {
+                let lowerBound = previousWordRange.upperBound
+                let upperBound = range.lowerBound
+                
+                if let range = rangeFromIndices(in: text, from: lowerBound, to: upperBound) {
+                    let substring = text[range]
+                    
+                    markedText += String(substring)
+                    currentPosition += substring.count
+                }
+            }
+            
+            let wordMark = "<mark name=\"w_\(currentPosition)_\(wordLength)\"/>"
+            markedText += "\(wordMark) \(word)"
+            previousWordRange = range
+            
+            currentPosition += wordLength
+            
+            return true
+        }
+        
+        return markedText
+    }
+    
+    func wrapWithSpeakTags(_ text: String) -> String {
+        return "<speak>\(text)</speak>"
+    }
+    
+    func rangeFromIndices(in text: String, from startIndex: String.Index, to endIndex: String.Index) -> Range<String.Index>? {
+        // 確保起始和結束索引是有效的
+        guard startIndex <= endIndex else {
+            return nil
+        }
+        // 返回字符串中的範圍
+        return startIndex..<endIndex
+    }
+    
+    func roundToOneDecimalPlace(_ value: Double) -> Double {
+        return round(value * 10) / 10
     }
 }
 
@@ -253,3 +347,13 @@ extension GoogleTTSService: AVAudioPlayerDelegate {
     }
 }
 
+struct TTSSynthesisResult {
+    let audioData: Data
+    let timepoints: [TimepointInfo]
+}
+
+struct TimepointInfo {
+    let range: NSRange?
+    let markName: String
+    let timeSeconds: Double
+}
